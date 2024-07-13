8b2f0c43f1718191275879b61baf01f1f3697eca
SuiXinSc <JiJinzhou123@outlook.com>
2024-07-13 17:28:15 +0800

    Update file

---
+++ SXGUI/ScreenDrivers/OLED_Driver/oled.c
@@ -0,0 +1,295 @@
+/********************************************************
+* @file     :oled.c
+* @writer   :岁心(SuiXinSc)
+*
+* @bilibili :https://space.bilibili.com/3494359452354953
+* @Gtihub   :https://github.com/SuiXinSc
+* @CSDN     :https://blog.csdn.net/m0_73677866
+*
+* @attention:
+*   Follow MIT License
+*   Copyright (c) 2024 SuiXinSc
+*********************************************************/
+
+#include "oled.h"
+#include "oledfont.h"
+#include "stdio.h"
+#include "gbk_font.h"
+#include <math.h>
+#include "stdlib.h"
+#include "string.h"
+
+unsigned char GRAM[3][1024+128*1+1]; //定义显存，适用于SRAM较大的单片机，多余的部分是缓冲区
+
+//写命令
+void OLED_WR_CMD(unsigned char cmd) {
+  /*SPI:*/
+  OLED_CS_Clr; //拉低片选表示选中OLED
+  OLED_DC_Clr; //把 D/C 引脚拉低以表示命令
+  HAL_SPI_Transmit(&hspi1,&cmd,1,1);
+  OLED_CS_Set;
+  /*IIC:
+  HAL_I2C_Mem_Write(&hi2c1 ,0x78,0x00,I2C_MEMADD_SIZE_8BIT,&cmd,1,1000);
+  */
+}
+
+//写数据
+void OLED_WR_Data(unsigned char* data) {
+  /*SPI:*/
+  OLED_CS_Clr; //拉低片选表示选中OLED
+  OLED_DC_Set; //把 D/C 引脚拉高以表示数据
+  HAL_SPI_Transmit(&hspi1,data,1024,1);
+  OLED_CS_Set;
+  /*IIC:
+  HAL_I2C_Mem_Write(&hi2c1 ,0x78,0x40,I2C_MEMADD_SIZE_8BIT,data,1024,1000);
+  */
+}
+
+
+void OLED_Init(void) {
+  HAL_Delay(200); //延时防止卡死
+  /*使用水平寻址模式*/
+  OLED_RES_Clr;
+  HAL_Delay(80);
+  OLED_RES_Set; //复位OLED
+  OLED_WR_CMD(0xAE); //display off
+  OLED_WR_CMD(0x20);	//Set Memory Addressing Mode
+  OLED_WR_CMD(0x00);	//00,Horizontal Addressing Mode;01,Vertical Addressing Mode;10,Page Addressing Mode (RESET);11,Invalid
+  OLED_WR_CMD(0xB0);	//Set Page Start Address for Page Addressing Mode,0-7
+  OLED_WR_CMD(0xC8);	//Set COM Output Scan Direction
+  OLED_WR_CMD(0x00); //---set low column address
+  OLED_WR_CMD(0x10); //---set high column address
+  OLED_WR_CMD(0x40); //--set start line address
+  OLED_WR_CMD(0x81); //--set contrast control register
+  OLED_WR_CMD(0x80); //亮度调节 0x00~0xff
+  OLED_WR_CMD(0xA1); //--set segment re-map 0 to 127
+  OLED_WR_CMD(0xA6); //--set normal display
+  OLED_WR_CMD(0xA8); //--set multiplex ratio(1 to 64)
+  OLED_WR_CMD(0x3F); //
+  OLED_WR_CMD(0xA4); //0xa4,Output follows RAM content;0xa5,Output ignores RAM content
+  OLED_WR_CMD(0xD3); //-set display offset
+  OLED_WR_CMD(0x00); //-not offset
+  OLED_WR_CMD(0xD5); //--set display clock divide ratio/oscillator frequency
+  OLED_WR_CMD(0xF0); //--set divide ratio
+  OLED_WR_CMD(0xD9); //--set pre-charge period
+  OLED_WR_CMD(0x22); //
+  OLED_WR_CMD(0xDA); //--set com pins hardware configuration
+  OLED_WR_CMD(0x12);
+  OLED_WR_CMD(0xDB); //--set vcomh
+  OLED_WR_CMD(0x20); //0x20,0.77xVcc
+  OLED_WR_CMD(0x8D); //--set DC-DC enable
+  OLED_WR_CMD(0x14); //
+  OLED_WR_CMD(0xAF); //--turn on oled panel
+}
+
+/*
+功能描述：清屏
+参数：layer: 要操作的层
+*/
+void OLED_Clear(int layer) {
+  if(layer == OLED_ALL_LAYER){
+    memset(GRAM[0]+128,0,1024);
+    memset(GRAM[1]+128,0,1024);
+    memset(GRAM[2]+128,0,1024);
+    return;
+  }
+  memset(GRAM[layer]+128,0,1024);
+}
+
+/*
+功能描述：在OLED中画点
+参数：x：x坐标；y：y坐标；mode:0,反白显示;1,正常显示;layer: 要操作的层
+*/
+void OLED_DrawPoint(int x,int y,int mode,int layer) {
+  if(x>127||y>63||x<0||y<0) {
+    return;
+  } //判断数据合法性
+  y+=8;   //跳过缓冲区
+  int line_y,pixel_y,temp; //定义临时变量
+  line_y = y / 8;  //计算对应显存行数
+  pixel_y = y % 8; //计算对应行的像素
+  temp = 0x01 << pixel_y; //通过移位得到数据
+  if(mode==1) {
+    GRAM[layer][line_y*128 + x] |= temp; //通过或运算来更新显存
+  } else {
+    GRAM[layer][line_y*128 + x] &= temp; //通过与运算来更新显存
+  }
+}
+
+/*
+功能描述：显示一个字符(包含字库中有的中文)
+参数：x(0~127)，y(0~63)：坐标；chr：字符；Size：尺寸12（6*8）、16（8*16）；mode:0,反白显示;1,正常显示;layer: 要操作的层
+*/
+void OLED_ShowChar(int x,int y,char* chr,int Size,int mode,int layer) {
+  y+=8;   //跳过缓冲区
+  unsigned char c=0;
+  if((int)chr[0]<128){
+    c=chr[0]-' ';                       //得到偏移后的值
+    for(int n=0;n<(int)(Size/8);n++){
+      if(mode == 1){
+        for(int i=0; i<Size/2; i++){
+          GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] &= 0xFF>>(8-(y%8));
+          GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] &= 0xFF<<(y%8);
+
+          GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] |= 
+            ASCII_Font[(int)(Size/8)-1][c*(Size/2*(int)(Size/8))+i+n*8]<<(y%8);
+          
+          GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] |= 
+            ASCII_Font[(int)(Size/8)-1][c*(Size/2*(int)(Size/8))+i+n*8]>>(8-(y%8));
+        }
+      }else{
+        for(int i=0; i<Size/2; i++){
+          GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] |= 0xFF<<(y%8);
+          GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] |= 0xFF>>(8-(y%8));
+          
+          GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] &= 
+            ~(ASCII_Font[(int)(Size/8)-1][c*(Size/2*(int)(Size/8))+i+n*8]<<(y%8));
+          
+          GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] &= 
+            ~(ASCII_Font[(int)(Size/8)-1][c*(Size/2*(int)(Size/8))+i+n*8]>>(8-(y%8)));
+        }
+      }
+      y+=8;
+    }
+  }else{
+    for(int i=0;i<sizeof(Index)/sizeof("中");i++){
+      if(memcmp(chr,Index[i],sizeof("中")-1)==0){
+        OLED_ShowBMP(x,y-8,(uint8_t*)Font16[i],16,16,mode,layer);
+        break;
+      }
+    }
+  }
+}
+
+/*
+功能描述：显示显示BMP图片
+参数: x(0~127), y(0~63): 起点坐标; *BMP:图片数组指针,BMP_Heigth: 图片高度;BMP_Width: 图片宽度;
+      mode:0,反白显示;1,正常显示;layer: 要操作的层
+*/
+void OLED_ShowBMP(int x,int y,uint8_t *BMP,int BMP_Heigth,int BMP_Width,int mode,int layer) {
+  y+=8;
+  for(int n=0;n<BMP_Heigth/8;n++){
+    if(mode == 1){
+      for(int i=0; i<BMP_Width; i++) {
+        GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] &= 0xFF>>(8-(y%8));
+        GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] &= 0xFF<<(y%8);
+        
+        GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] |= 
+          BMP[n*BMP_Width+i]<<(y%8);
+        
+        GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] |= 
+          BMP[n*BMP_Width+i]>>(8-(y%8));
+      }
+    }else{
+      for(int i=0; i<BMP_Width; i++) {
+        GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] |= 0xFF<<(y%8);
+        GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] |= 0xFF>>(8-(y%8));
+        
+        GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] &= 
+          ~(BMP[n*BMP_Width+i]<<(y%8));
+        
+        GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] &= 
+          ~(BMP[n*BMP_Width+i]>>(8-(y%8)));
+      }
+    }
+    y+=8;
+  }
+}
+
+//画四分之一圆
+void OLED_DrawQuarterRound(int xo,int yo,int r,int width,int quarter,uint32_t color,int layer){
+  int xs = Max(xo, 0);
+  int xe = Min(xo + r + 1, 127);
+  int ys = Max(yo, 0);
+  int ye = Min(yo + r + 1, 63);
+  
+  for (int i=ys;i<ye;i++){
+    for (int t=xs-ys+i;t<xe;t++){
+      if (fabs(powf(xo-t,2)+powf(yo-i,2))<=powf(r,2)){
+        switch(quarter){
+          case 1:OLED_DrawPoint(t,2*ys-i,color,layer);
+            OLED_DrawPoint(xs+i-ys,ys+xs-t,color,layer);
+            break;
+          case 2:OLED_DrawPoint(2*xs-t,2*ys-i,color,layer);
+            OLED_DrawPoint(xs+ys-i,ys+xs-t,color,layer);
+            break;
+          case 3:OLED_DrawPoint(2*xs-t,i,color,layer);
+            OLED_DrawPoint(xs+ys-i,ys+t-xs,color,layer);
+            break;
+          case 4:OLED_DrawPoint(t,i,color,layer);
+            OLED_DrawPoint(xs+i-ys,ys+t-xs,color,layer);
+            break;
+          default:break;
+        }
+      }
+    }
+  }
+}
+
+//反色显示
+void OLED_Negation(int ixs,int iys,int ixe,int iye,int mode){
+  int xs=Min(ixs,ixe),ys=Min(iys,iye),
+      xe=Max(ixs,ixe),ye=Max(iys,iye);      //重新整理起始点和结束点
+  
+  OLED_Clear(OLED_IMAGE_LAYER);
+  
+  if(mode){       //圆角
+    OLED_DrawQuarterRound(xs+2,ys+2,2,1,2,1,OLED_IMAGE_LAYER);
+    OLED_DrawQuarterRound(xe-2,ys+2,2,1,1,1,OLED_IMAGE_LAYER);
+    OLED_DrawQuarterRound(xs+2,ye-2,2,1,3,1,OLED_IMAGE_LAYER);
+    OLED_DrawQuarterRound(xe-2,ye-2,2,1,4,1,OLED_IMAGE_LAYER);
+    
+    for(int i=ys+2;i<=ye-2;i++){
+      for(int t=xs;t<=xe;t++){
+        OLED_DrawPoint(t,i,1,OLED_IMAGE_LAYER);
+      }
+    }
+    for(int i=ys;i<=ys+2;i++){
+      for(int t=xs+2;t<=xe-2;t++){
+        OLED_DrawPoint(t,i,1,OLED_IMAGE_LAYER);
+      }
+    }
+    for(int i=ye-2;i<=ye;i++){
+      for(int t=xs+2;t<=xe-2;t++){
+        OLED_DrawPoint(t,i,1,OLED_IMAGE_LAYER);
+      }
+    }
+    
+  }else{
+    for(int i=ys;i<=ye;i++){
+      for(int t=xs;t<=xe;t++){
+        OLED_DrawPoint(t,i,1,OLED_IMAGE_LAYER);
+      }
+    }
+  }
+  
+  for(int i=0;i<1024;i++){
+    GRAM[OLED_DATA_LAYER][i+128] ^= GRAM[OLED_IMAGE_LAYER][i+128];
+  }
+  OLED_Clear(OLED_IMAGE_LAYER);
+}
+
+//或运算更新显存
+void OLED_UpDateToGRAM(uint8_t Data[1024],int layer){
+  for(int i=0;i<1024;i++){
+    GRAM[layer][i+128]|=Data[i];
+  }
+}
+
+//获取显存
+void OLED_GetGRAM(uint8_t *Data,int layer){
+  memcpy(Data,GRAM[layer]+128,1024);
+}
+
+//光栅运算
+void OLED_Grating_Count(){
+  for(int i=0;i<1024;i++){
+    GRAM[OLED_DATA_LAYER][i+128] &= ~GRAM[OLED_GRATING_LAYER][i+128];
+    GRAM[OLED_DATA_LAYER][i+128] |= GRAM[OLED_IMAGE_LAYER][i+128]&GRAM[OLED_GRATING_LAYER][i+128];
+  }
+}
+
+//发送显存
+void OLED_GRAM_Transmit(void) {
+  OLED_WR_Data(GRAM[OLED_DATA_LAYER]+128);
+}
---
+++ SXGUI_Demo/Core/Src/main.c
@@ -0,0 +1,403 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "spi.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "sxgui.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+//APP函数
+void Draw_Line(SXGUI_KeyItem *Key){
+  static int tick;
+  static int i;
+  if(Graphics_GetTick()-tick>15){
+    i++;
+    tick = Graphics_GetTick();
+  }
+  if(i>127){
+    i=0;
+  }
+  Graphics_DrawLine(0,0,i,63,1,SOLID_LINE);
+  Graphics_DrawLine(127,0,127-i,63,1,DASHED_LINE);
+}
+
+void Draw_Round(SXGUI_KeyItem *Key){
+  static int tick;
+  static int i;
+  static bool flag = true;
+  if(Graphics_GetTick()-tick>30){
+    if(flag){
+      i++;
+    }else{
+      i--;
+    }
+    tick = Graphics_GetTick();
+  }
+  if(i>31){
+    flag = false;
+  }else if(i<0){
+    flag = true;
+  }
+  char tmp[8];
+  sprintf(tmp,"r=%d",i);
+  Graphics_DrawRound(64,31,i,1,1,SOLID);
+  Graphics_ShowString(0,0,tmp,16,1);
+}
+
+void Round_Rect(SXGUI_KeyItem *Key){
+  static int tick;
+  static int width=0;
+  static bool flag = true;
+  if(Graphics_GetTick()-tick>15){
+    if(flag){
+      width++;
+    }else{
+      width--;
+    }
+    tick = Graphics_GetTick();
+  }
+  if(width>63){
+    flag = false;
+  }else if(width<0){
+    flag = true;
+  }
+  Graphics_DrawRoundRect(0,0,width,width/2,4,1,HOLLOW);
+  Graphics_DrawRoundRect(64,0,width+64,width/2,4,1,SOLID);
+  Graphics_DrawRoundRect(0,32,width,width/2+32,8,1,SOLID);
+  Graphics_DrawRoundRect(64,32,width+64,width/2+32,8,1,HOLLOW);
+}
+
+void Kernel_Infor(SXGUI_KeyItem *Key){
+  Graphics_ShowString(0,24,"内核版本:v1.0",16,WHITE_COLOR);
+}
+
+void GUI_Infor(SXGUI_KeyItem *Key){
+  Graphics_ShowString(0,24,"SXGUI v1.0",16,WHITE_COLOR);
+}
+
+void Graphics_Infor(SXGUI_KeyItem *Key){
+  Graphics_ShowString(0,24,"图形库版本:v1.1",16,WHITE_COLOR);
+}
+
+void Driver_Infor(SXGUI_KeyItem *Key){
+  Graphics_ShowString(0,24,"驱动版本:v1.3.M",16,WHITE_COLOR);
+}
+
+void Writer_Infor(SXGUI_KeyItem *Key){
+  static int tick;
+  static bool CB = true;
+  Graphics_ShowString(0,0,"作者:岁心",16,WHITE_COLOR);
+  if(Graphics_GetTick()-tick>5000){
+    CB = !CB;
+    tick = Graphics_GetTick();
+  }
+  if(CB){
+    Graphics_ShowString(0,24,"CSDN: https://blog.csdn.net/m0_73677866",12,WHITE_COLOR);
+    return;
+  }
+  Graphics_ShowString(0,24,"Bilibili: https://space.bilibili.com/3494359452354953",12,WHITE_COLOR);
+}
+
+void PID_Infor(SXGUI_KeyItem *Key){
+  char tmp[16];
+  sprintf(tmp,"Kp: %.3f",PID_Parameter.Kp);
+  Graphics_ShowString(0,0,tmp,12,WHITE_COLOR);
+  sprintf(tmp,"Ki: %.3f",PID_Parameter.Ki);
+  Graphics_ShowString(0,12,tmp,12,WHITE_COLOR);
+  sprintf(tmp,"Kd: %.3f",PID_Parameter.Kd);
+  Graphics_ShowString(0,24,tmp,12,WHITE_COLOR);
+  sprintf(tmp,"Factor: %.3f",PID_Parameter.Factor);
+  Graphics_ShowString(0,36,tmp,12,WHITE_COLOR);
+  sprintf(tmp,"MinTime: %dms",PID_Parameter.MinTime);
+  Graphics_ShowString(0,48,tmp,12,WHITE_COLOR);
+}
+
+//调用SXGUI的库函数, 初始化并创建GUI界面
+void SX_MenuInit(void){
+  SXGUI_Init("Hub");
+  SXGUI_MenuItem *Sub1 = SXGUI_CreateMenu("信息");
+  SXGUI_MenuItem *Sub2 = SXGUI_CreateMenu("GUI信息");
+  SXGUI_APPItem *App1 = SXGUI_CreateApp("Draw Line",Draw_Line);
+  SXGUI_APPItem *App2 = SXGUI_CreateApp("Draw Round",Draw_Round);
+  SXGUI_APPItem *App3 = SXGUI_CreateApp("Round Rect",Round_Rect);
+  SXGUI_APPItem *App4 = SXGUI_CreateApp("内核版本",Kernel_Infor);
+  SXGUI_APPItem *App5 = SXGUI_CreateApp("GUI版本",GUI_Infor);
+  SXGUI_APPItem *App6 = SXGUI_CreateApp("图形库版本",Graphics_Infor);
+  SXGUI_APPItem *App7 = SXGUI_CreateApp("驱动信息",Driver_Infor);
+  SXGUI_APPItem *App8 = SXGUI_CreateApp("作者信息",Writer_Infor);
+  SXGUI_APPItem *App9 = SXGUI_CreateApp("PID Parameter",PID_Infor);
+  SXGUI_RootAddSubMenu(Sub1);
+  SXGUI_RootAddApp(App1);
+  SXGUI_RootAddApp(App2);
+  SXGUI_RootAddApp(App3);
+  SXGUI_AddSubMenu(Sub1,Sub2);
+  SXGUI_AddApp(Sub2,App4);
+  SXGUI_AddApp(Sub2,App5);
+  SXGUI_AddApp(Sub2,App6);
+  SXGUI_AddApp(Sub2,App7);
+  SXGUI_AddApp(Sub2,App8);
+  SXGUI_AddApp(Sub2,App9);
+}
+
+//读取按键并消抖
+#define KeyNum    4
+void Scan_Key(SXGUI_KeyItem *Key){
+  static int ShakeFree[KeyNum];
+  static int tick;
+  static SXGUI_KeyItem Last;
+  if(HAL_GetTick()-tick>2){
+    tick = HAL_GetTick();
+  }else{
+    return;
+  }
+  bool *KeyPtr;
+  GPIO_PinState PINState;
+  SXGUI_KeyItem tmp;
+  for(int i=0;i<KeyNum;i++){
+    switch(i){
+      case 0:KeyPtr = &Key->Back;
+        PINState = HAL_GPIO_ReadPin(GPIOC,BACK_KEY_Pin);
+        break;
+      case 1:KeyPtr = &Key->Pre;
+        PINState = HAL_GPIO_ReadPin(GPIOC,PRE_KEY_Pin);
+        break;
+      case 2:KeyPtr = &Key->Next;
+        PINState = HAL_GPIO_ReadPin(GPIOC,NEXT_KEY_Pin);
+        break;
+      case 3:KeyPtr = &Key->OK;
+        PINState = HAL_GPIO_ReadPin(GPIOC,OK_KEY_Pin);
+        break;
+    }
+    if(PINState==RESET){
+      ShakeFree[i]++;
+      if(ShakeFree[i]>5){
+        *KeyPtr = true;
+        ShakeFree[i] = 10;
+      }else{
+        *KeyPtr = false;
+      }
+    }else{
+      ShakeFree[i]--;
+      if(ShakeFree[i]<5){
+        ShakeFree[i]=0;
+        *KeyPtr = false;
+      }else{
+        *KeyPtr = true;
+      }
+    }
+  }
+  tmp.Back = Key->Back;
+  tmp.Pre = Key->Pre;
+  tmp.Next = Key->Next;
+  tmp.OK = Key->OK;
+  
+  Key->Back ^= Last.Back;
+  Key->Pre ^= Last.Pre;
+  Key->Next ^= Last.Next;
+  Key->OK ^= Last.OK;
+  
+  Key->Back &= tmp.Back;
+  Key->Pre &= tmp.Pre;
+  Key->Next &= tmp.Next;
+  Key->OK &= tmp.OK;
+  
+  Last.Back = tmp.Back;
+  Last.Pre = tmp.Pre;
+  Last.Next = tmp.Next;
+  Last.OK = tmp.OK;
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  /* USER CODE BEGIN 2 */
+  //设置PID参数
+  PID_Parameter.Kp = -3.2;
+  PID_Parameter.Ki = 4.032;
+  PID_Parameter.Kd = 0.879;
+  PID_Parameter.Factor = 0.1;
+  PID_Parameter.MinTime = 10;
+  
+  //创建按键结构体
+  SXGUI_KeyItem *Key = malloc(sizeof(SXGUI_KeyItem));
+  
+  //初始化图形库和GUI库
+  Graphics_Init();
+  SX_MenuInit();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    //主程序执行
+    Scan_Key(Key);
+    SXGUI_Main(16,BLACK_COLOR,WHITE_COLOR,FRAME_STYLE,Key);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
