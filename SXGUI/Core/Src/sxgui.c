2a7fc3237e66e86bdb1a5fd9708119a2391349ad
SuiXinSc <JiJinzhou123@outlook.com>
2024-07-14 13:13:03 +0800

    Update file

--- SXGUI/Core/Src/sxgui.c
+++ SXGUI/Core/Src/sxgui.c
@@ -23,24 +23,23 @@
 #define INTERFACE_Y_ADDER     0
 
 //绘制界面, 可根据需要更改
-void SXGUI_Interface(int Fontsize,SXGUI_MenuItem *Menu,int option,int MenuNum,
-                    int AppNum,uint32_t BackColor,uint32_t FontColor,int style)
-{
-  int List_x = INTERFACE_X_ADDER + Fontsize/2,
+void SXGUI_Interface(int Fontsize, SXGUI_MenuItem* Menu, int option, int MenuNum,
+                     int AppNum, uint32_t BackColor, uint32_t FontColor, int style) {
+  int List_x = INTERFACE_X_ADDER + Fontsize / 2,
       List_y = INTERFACE_Y_ADDER + Fontsize + 1;
-  
+
   Graphics_ListChoose(List_x, List_y, INTERFACE_WIDTH - List_x - 1, INTERFACE_HEIGHT - Fontsize - 1,
-                        Fontsize,Menu,MenuNum,AppNum,option,style,FontColor);
-  
-  Graphics_ShowString(INTERFACE_X_ADDER,INTERFACE_Y_ADDER,Menu->name,Fontsize,FontColor);
+                      Fontsize, Menu, MenuNum, AppNum, option, style, FontColor);
+
+  Graphics_ShowString(INTERFACE_X_ADDER, INTERFACE_Y_ADDER, Menu->name, Fontsize, FontColor);
 }
 
 //根目录
-SXGUI_MenuItem *ROOT;
+SXGUI_MenuItem* ROOT;
 
 //创建根
-int SXGUI_CreateRoot(char *Name){
-  if(ROOT != NULL){
+int SXGUI_CreateRoot(char* Name) {
+  if(ROOT != NULL) {
     return SXGUI_ERROR;     //如果已创建
   }
   ROOT = malloc(sizeof(SXGUI_MenuItem));
@@ -49,33 +48,33 @@ int SXGUI_CreateRoot(char *Name){
 }
 
 //创建菜单项
-SXGUI_MenuItem *SXGUI_CreateMenu(char *Name){
-  SXGUI_MenuItem *Menu = malloc(sizeof(SXGUI_MenuItem));
+SXGUI_MenuItem* SXGUI_CreateMenu(char* Name) {
+  SXGUI_MenuItem* Menu = malloc(sizeof(SXGUI_MenuItem));
   Menu->name = Name;
   return Menu;
 }
 
 //创建APP项
-SXGUI_APPItem *SXGUI_CreateApp(char *Name,void (*AppFunction)(struct SXGUI_KeyItem *Key)){
-  SXGUI_APPItem *APP = malloc(sizeof(SXGUI_APPItem));
+SXGUI_APPItem* SXGUI_CreateApp(char* Name, void (*AppFunction)(struct SXGUI_KeyItem* Key)) {
+  SXGUI_APPItem* APP = malloc(sizeof(SXGUI_APPItem));
   APP->name = Name;
   APP->Function = AppFunction;
   return APP;
 }
 
 //释放GUI
-void SXGUI_Free(void){
+void SXGUI_Free(void) {
   free(ROOT);
 }
 
 //删除菜单项
-void SXGUI_DeleteMenu(SXGUI_MenuItem *Menu){
-  if(Menu->Pre == NULL){
+void SXGUI_DeleteMenu(SXGUI_MenuItem* Menu) {
+  if(Menu->Pre == NULL) {
     Menu->Parent->Sub = Menu->Next;
     Menu->Next->Pre = NULL;
-  }else{
+  } else {
     Menu->Pre->Next = Menu->Next;
-    if(Menu->Next!=NULL){
+    if(Menu->Next != NULL) {
       Menu->Next->Pre = Menu->Pre;
     }
   }
@@ -83,13 +82,13 @@ void SXGUI_DeleteMenu(SXGUI_MenuItem *Menu){
 }
 
 //删除APP项
-void SXGUI_DeleteApp(SXGUI_APPItem *APP){
-  if(APP->Pre == NULL){
+void SXGUI_DeleteApp(SXGUI_APPItem* APP) {
+  if(APP->Pre == NULL) {
     APP->Parent->App = APP->Next;
     APP->Next->Pre = NULL;
-  }else{
+  } else {
     APP->Pre->Next = APP->Next;
-    if(APP->Next!=NULL){
+    if(APP->Next != NULL) {
       APP->Next->Pre = APP->Pre;
     }
   }
@@ -97,159 +96,159 @@ void SXGUI_DeleteApp(SXGUI_APPItem *APP){
 }
 
 //在根目录下添加菜单项
-int SXGUI_RootAddSubMenu(SXGUI_MenuItem *SubMenu){
-  if(SubMenu->Parent!=NULL){    //如果已被添加
+int SXGUI_RootAddSubMenu(SXGUI_MenuItem* SubMenu) {
+  if(SubMenu->Parent != NULL) { //如果已被添加
     return SXGUI_ERROR;
   }
-  
+
   SubMenu->Parent = ROOT;
   SXGUI_MenuItem* Ptr = ROOT->Sub;
-  
-  if(Ptr!=NULL){
-    for(;Ptr->Next!=NULL;Ptr=Ptr->Next);
+
+  if(Ptr != NULL) {
+    for(; Ptr->Next != NULL; Ptr = Ptr->Next);
     Ptr->Next = SubMenu;
-  }else{
+  } else {
     ROOT->Sub = SubMenu;
   }
-  
+
   SubMenu->Pre = Ptr;
-  
+
   return SXGUI_OK;
 }
 
 //根目录下添加APP项
-int SXGUI_RootAddApp(SXGUI_APPItem *APP){
-  if(APP->Parent!=NULL){      //如果已被添加
+int SXGUI_RootAddApp(SXGUI_APPItem* APP) {
+  if(APP->Parent != NULL) {   //如果已被添加
     return SXGUI_ERROR;
   }
   APP->Parent = ROOT;
-  
+
   SXGUI_APPItem* Ptr = ROOT->App;
-  
-  if(Ptr!=NULL){
-    for(;Ptr->Next!=NULL;Ptr=Ptr->Next);
+
+  if(Ptr != NULL) {
+    for(; Ptr->Next != NULL; Ptr = Ptr->Next);
     Ptr->Next = APP;
-  }else{
+  } else {
     ROOT->App = APP;
   }
-  
+
   APP->Pre = Ptr;
-  
+
   return SXGUI_OK;
 }
 
 //菜单下添加子菜单
-int SXGUI_AddSubMenu(SXGUI_MenuItem *ParentMenu, SXGUI_MenuItem *SubMenu){
-  if(SubMenu->Parent!=NULL){      //如果已被添加
+int SXGUI_AddSubMenu(SXGUI_MenuItem* ParentMenu, SXGUI_MenuItem* SubMenu) {
+  if(SubMenu->Parent != NULL) {   //如果已被添加
     return SXGUI_ERROR;
   }
   SubMenu->Parent = ParentMenu;
-  
+
   SXGUI_MenuItem* Ptr = ParentMenu->Sub;
-  
-  if(Ptr!=NULL){
-    for(;Ptr->Next!=NULL;Ptr=Ptr->Next);
+
+  if(Ptr != NULL) {
+    for(; Ptr->Next != NULL; Ptr = Ptr->Next);
     Ptr->Next = SubMenu;
-  }else{
+  } else {
     ParentMenu->Sub = SubMenu;
   }
-  
+
   SubMenu->Pre = Ptr;
-  
+
   return SXGUI_OK;
 }
 
 //菜单下添加APP
-int SXGUI_AddApp(SXGUI_MenuItem *ParentMenu, SXGUI_APPItem *APP){
-  if(APP->Parent!=NULL){      //如果已被添加
+int SXGUI_AddApp(SXGUI_MenuItem* ParentMenu, SXGUI_APPItem* APP) {
+  if(APP->Parent != NULL) {   //如果已被添加
     return SXGUI_ERROR;
   }
   APP->Parent = ParentMenu;
-  
+
   SXGUI_APPItem* Ptr = ParentMenu->App;
-  
-  if(Ptr!=NULL){
-    for(;Ptr->Next!=NULL;Ptr=Ptr->Next);
+
+  if(Ptr != NULL) {
+    for(; Ptr->Next != NULL; Ptr = Ptr->Next);
     Ptr->Next = APP;
-  }else{
+  } else {
     ParentMenu->App = APP;
   }
-  
+
   APP->Pre = Ptr;
-  
+
   return SXGUI_OK;
 }
 
 //指针变量
-SXGUI_MenuItem *NowMenu;
-SXGUI_MenuItem *MenuSelect;
-SXGUI_APPItem *AppSelect;
+SXGUI_MenuItem* NowMenu;
+SXGUI_MenuItem* MenuSelect;
+SXGUI_APPItem* AppSelect;
 
 //GUI初始化
-void SXGUI_Init(char *HubName){
+void SXGUI_Init(char* HubName) {
   SXGUI_CreateRoot(HubName);    //创建根目录
   NowMenu = ROOT;     //初始化指针变量
 }
 
-void SXGUI_Main(int Fontsize,uint32_t BackColor,uint32_t FontColor,int style,SXGUI_KeyItem *Key){
+void SXGUI_Main(int Fontsize, uint32_t BackColor, uint32_t FontColor, int style, SXGUI_KeyItem* Key) {
   static int option = 0;
   static int tick = 0;
   static bool Success_flag = true;
   static bool RunApp = false;
-  static bool init_flag=true;
-  
-  if(init_flag){
-    if(NowMenu->Sub!=NULL){
+  static bool init_flag = true;
+
+  if(init_flag) {
+    if(NowMenu->Sub != NULL) {
       MenuSelect = NowMenu->Sub;
       AppSelect = NULL;
-    }else{
+    } else {
       MenuSelect = NULL;
       AppSelect = NowMenu->App;
     }
-    init_flag=false;
+    init_flag = false;
   }
-  
+
   int MenuLinkListNum;
   int AppLinkListNum;
-  
-  SXGUI_MenuItem *Ptr = NowMenu->Sub;
-  SXGUI_APPItem *AppPtr = NowMenu->App;
-  
+
+  SXGUI_MenuItem* Ptr = NowMenu->Sub;
+  SXGUI_APPItem* AppPtr = NowMenu->App;
+
   //计算子菜单项数量和APP数量
-  for(MenuLinkListNum=0;Ptr!=NULL;Ptr = Ptr->Next,MenuLinkListNum++);
-  for(AppLinkListNum=0;AppPtr!=NULL;AppPtr = AppPtr->Next,AppLinkListNum++);
-  
+  for(MenuLinkListNum = 0; Ptr != NULL; Ptr = Ptr->Next, MenuLinkListNum++);
+  for(AppLinkListNum = 0; AppPtr != NULL; AppPtr = AppPtr->Next, AppLinkListNum++);
+
   /*按键处理*/
-  
+
   //确定
-  if(Key->OK&&Success_flag){
-    if(MenuSelect!=NULL){
+  if(Key->OK && Success_flag) {
+    if(MenuSelect != NULL) {
       NowMenu = MenuSelect;
       MenuSelect = MenuSelect->Sub;
-      if(MenuSelect!=NULL){
+      if(MenuSelect != NULL) {
         tick = Graphics_GetTick();
         Success_flag = false;
         AppSelect = NULL;
-      }else{
+      } else {
         AppSelect = NowMenu->App;
         tick = Graphics_GetTick();
         Success_flag = false;
       }
       option = 0;
-    }else if(AppSelect!=NULL&&RunApp!=true){
+    } else if(AppSelect != NULL && RunApp != true) {
       tick = Graphics_GetTick();
       Success_flag = false;
       RunApp = true;
     }
   }
-  
+
   //回退
-  if(Key->Back&&Success_flag){
-    if(RunApp){
+  if(Key->Back && Success_flag) {
+    if(RunApp) {
       tick = Graphics_GetTick();
       Success_flag = false;
       RunApp = false;
-    }else if(NowMenu->Parent!=NULL){
+    } else if(NowMenu->Parent != NULL) {
       tick = Graphics_GetTick();
       Success_flag = false;
       NowMenu = NowMenu->Parent;
@@ -260,95 +259,95 @@ void SXGUI_Main(int Fontsize,uint32_t BackColor,uint32_t FontColor,int style,SXG
   }
 
   //下一个
-  if(Key->Next&&RunApp==false&&Success_flag){
-    if(MenuSelect!=NULL){
-      if(MenuSelect->Next!=NULL){
+  if(Key->Next && RunApp == false && Success_flag) {
+    if(MenuSelect != NULL) {
+      if(MenuSelect->Next != NULL) {
         AppSelect = NULL;
         MenuSelect = MenuSelect->Next;
         option++;
-      }else if(NowMenu->App!=NULL){
+      } else if(NowMenu->App != NULL) {
         AppSelect = NowMenu->App;
         MenuSelect = NULL;
         option++;
-      }else{
+      } else {
         AppSelect = NULL;
         MenuSelect = NowMenu->Sub;
-        option=0;
+        option = 0;
       }
-    }else if(AppSelect!=NULL){
-      if(AppSelect->Next!=NULL){
+    } else if(AppSelect != NULL) {
+      if(AppSelect->Next != NULL) {
         AppSelect = AppSelect->Next;
         MenuSelect = NULL;
         option++;
-      }else if(NowMenu->Sub!=NULL){
+      } else if(NowMenu->Sub != NULL) {
         AppSelect = NULL;
         MenuSelect = NowMenu->Sub;
-        option=0;
-      }else{
+        option = 0;
+      } else {
         AppSelect = NowMenu->App;
         MenuSelect = NULL;
-        option=0;
+        option = 0;
       }
     }
   }
-  
+
   //上一个
-  if(Key->Pre&&RunApp==false&&Success_flag){
-    if(MenuSelect!=NULL){
-      if(MenuSelect->Pre!=NULL){
+  if(Key->Pre && RunApp == false && Success_flag) {
+    if(MenuSelect != NULL) {
+      if(MenuSelect->Pre != NULL) {
         AppSelect = NULL;
         MenuSelect = MenuSelect->Pre;
         option--;
-      }else if(NowMenu->App!=NULL){
+      } else if(NowMenu->App != NULL) {
         AppPtr = NowMenu->App;
-        for(;AppPtr->Next!=NULL;AppPtr=AppPtr->Next);
+        for(; AppPtr->Next != NULL; AppPtr = AppPtr->Next);
         AppSelect = AppPtr;
         MenuSelect = NULL;
-        option = AppLinkListNum+MenuLinkListNum-1;
-      }else{
+        option = AppLinkListNum + MenuLinkListNum - 1;
+      } else {
         AppSelect = NULL;
         Ptr = NowMenu->Sub;
-        for(;Ptr->Next!=NULL;Ptr=Ptr->Next);
+        for(; Ptr->Next != NULL; Ptr = Ptr->Next);
         MenuSelect = Ptr;
-        option = AppLinkListNum+MenuLinkListNum-1;
+        option = AppLinkListNum + MenuLinkListNum - 1;
       }
-    }else if(AppSelect!=NULL){
-      if(AppSelect->Pre!=NULL){
+    } else if(AppSelect != NULL) {
+      if(AppSelect->Pre != NULL) {
         AppSelect = AppSelect->Pre;
         MenuSelect = NULL;
         option--;
-      }else if(NowMenu->Sub!=NULL){
+      } else if(NowMenu->Sub != NULL) {
         AppSelect = NULL;
         Ptr = NowMenu->Sub;
-        for(;Ptr->Next!=NULL;Ptr=Ptr->Next);
+        for(; Ptr->Next != NULL; Ptr = Ptr->Next);
         MenuSelect = Ptr;
         option--;
-      }else{
+      } else {
         AppPtr = NowMenu->App;
-        for(;AppPtr->Next!=NULL;AppPtr=AppPtr->Next);
+        for(; AppPtr->Next != NULL; AppPtr = AppPtr->Next);
         AppSelect = AppPtr;
         MenuSelect = NULL;
-        option = AppLinkListNum+MenuLinkListNum-1;
+        option = AppLinkListNum + MenuLinkListNum - 1;
       }
     }
   }
-  
+
   //执行APP/绘制UI界面
-  if(RunApp&&AppSelect->Function!=NULL){
+  if(RunApp && AppSelect->Function != NULL) {
     AppSelect->Function(Key);
-  }else{
-    SXGUI_Interface(Fontsize,NowMenu,option,MenuLinkListNum,AppLinkListNum,BackColor,FontColor,style);
+  } else {
+    SXGUI_Interface(Fontsize, NowMenu, option, MenuLinkListNum, AppLinkListNum, BackColor, FontColor, style);
   }
-  
+
   //虚化效果
-  if(Graphics_GetTick()-tick<TRANSITION_TIMES/3){
+  if(Graphics_GetTick() - tick < TRANSITION_TIMES / 3) {
     Graphics_Clear();
-  }else if(Graphics_GetTick()-tick<TRANSITION_TIMES){
+  } else if(Graphics_GetTick() - tick < TRANSITION_TIMES) {
     Graphics_Bokeh();
-  }else{
+  } else {
     Success_flag = true;
   }
-  
+
   //在屏幕上显示
   Graphics_Display();
 }
--- SXGUI/ScreenDrivers/OLED_Driver/oled.c
+++ SXGUI/ScreenDrivers/OLED_Driver/oled.c
@@ -19,14 +19,14 @@
 #include "stdlib.h"
 #include "string.h"
 
-unsigned char GRAM[3][1024+128*1+1]; //定义显存，适用于SRAM较大的单片机，多余的部分是缓冲区
+unsigned char GRAM[3][1024 + 128 * 1 + 1]; //定义显存，适用于SRAM较大的单片机，多余的部分是缓冲区
 
 //写命令
 void OLED_WR_CMD(unsigned char cmd) {
   /*SPI:*/
   OLED_CS_Clr; //拉低片选表示选中OLED
   OLED_DC_Clr; //把 D/C 引脚拉低以表示命令
-  HAL_SPI_Transmit(&hspi1,&cmd,1,1);
+  HAL_SPI_Transmit(&hspi1, &cmd, 1, 1);
   OLED_CS_Set;
   /*IIC:
   HAL_I2C_Mem_Write(&hi2c1 ,0x78,0x00,I2C_MEMADD_SIZE_8BIT,&cmd,1,1000);
@@ -38,7 +38,7 @@ void OLED_WR_Data(unsigned char* data) {
   /*SPI:*/
   OLED_CS_Clr; //拉低片选表示选中OLED
   OLED_DC_Set; //把 D/C 引脚拉高以表示数据
-  HAL_SPI_Transmit(&hspi1,data,1024,1);
+  HAL_SPI_Transmit(&hspi1, data, 1024, 1);
   OLED_CS_Set;
   /*IIC:
   HAL_I2C_Mem_Write(&hi2c1 ,0x78,0x40,I2C_MEMADD_SIZE_8BIT,data,1024,1000);
@@ -87,32 +87,32 @@ void OLED_Init(void) {
 参数：layer: 要操作的层
 */
 void OLED_Clear(int layer) {
-  if(layer == OLED_ALL_LAYER){
-    memset(GRAM[0]+128,0,1024);
-    memset(GRAM[1]+128,0,1024);
-    memset(GRAM[2]+128,0,1024);
+  if(layer == OLED_ALL_LAYER) {
+    memset(GRAM[0] + 128, 0, 1024);
+    memset(GRAM[1] + 128, 0, 1024);
+    memset(GRAM[2] + 128, 0, 1024);
     return;
   }
-  memset(GRAM[layer]+128,0,1024);
+  memset(GRAM[layer] + 128, 0, 1024);
 }
 
 /*
 功能描述：在OLED中画点
 参数：x：x坐标；y：y坐标；mode:0,反白显示;1,正常显示;layer: 要操作的层
 */
-void OLED_DrawPoint(int x,int y,int mode,int layer) {
-  if(x>127||y>63||x<0||y<0) {
+void OLED_DrawPoint(int x, int y, int mode, int layer) {
+  if(x > 127 || y > 63 || x < 0 || y < 0) {
     return;
   } //判断数据合法性
-  y+=8;   //跳过缓冲区
-  int line_y,pixel_y,temp; //定义临时变量
+  y += 8; //跳过缓冲区
+  int line_y, pixel_y, temp; //定义临时变量
   line_y = y / 8;  //计算对应显存行数
   pixel_y = y % 8; //计算对应行的像素
   temp = 0x01 << pixel_y; //通过移位得到数据
-  if(mode==1) {
-    GRAM[layer][line_y*128 + x] |= temp; //通过或运算来更新显存
+  if(mode == 1) {
+    GRAM[layer][line_y * 128 + x] |= temp; //通过或运算来更新显存
   } else {
-    GRAM[layer][line_y*128 + x] &= temp; //通过与运算来更新显存
+    GRAM[layer][line_y * 128 + x] &= temp; //通过与运算来更新显存
   }
 }
 
@@ -120,41 +120,41 @@ void OLED_DrawPoint(int x,int y,int mode,int layer) {
 功能描述：显示一个字符(包含字库中有的中文)
 参数：x(0~127)，y(0~63)：坐标；chr：字符；Size：尺寸12（6*8）、16（8*16）；mode:0,反白显示;1,正常显示;layer: 要操作的层
 */
-void OLED_ShowChar(int x,int y,char* chr,int Size,int mode,int layer) {
-  y+=8;   //跳过缓冲区
-  unsigned char c=0;
-  if((int)chr[0]<128){
-    c=chr[0]-' ';                       //得到偏移后的值
-    for(int n=0;n<(int)(Size/8);n++){
-      if(mode == 1){
-        for(int i=0; i<Size/2; i++){
-          GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] &= 0xFF>>(8-(y%8));
-          GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] &= 0xFF<<(y%8);
+void OLED_ShowChar(int x, int y, char* chr, int Size, int mode, int layer) {
+  y += 8; //跳过缓冲区
+  unsigned char c = 0;
+  if((int)chr[0] < 128) {
+    c = chr[0] - ' ';                   //得到偏移后的值
+    for(int n = 0; n < (int)(Size / 8); n++) {
+      if(mode == 1) {
+        for(int i = 0; i < Size / 2; i++) {
+          GRAM[layer][Max(0, Min((int)(y / 8) * 128 + x + i, 1153))] &= 0xFF >> (8 - (y % 8));
+          GRAM[layer][Max(0, Min(((int)(y / 8) + 1) * 128 + x + i, 1153))] &= 0xFF << (y % 8);
 
-          GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] |= 
-            ASCII_Font[(int)(Size/8)-1][c*(Size/2*(int)(Size/8))+i+n*8]<<(y%8);
-          
-          GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] |= 
-            ASCII_Font[(int)(Size/8)-1][c*(Size/2*(int)(Size/8))+i+n*8]>>(8-(y%8));
+          GRAM[layer][Max(0, Min((int)(y / 8) * 128 + x + i, 1153))] |=
+            ASCII_Font[(int)(Size / 8) - 1][c * (Size / 2 * (int)(Size / 8)) + i + n * 8] << (y % 8);
+
+          GRAM[layer][Max(0, Min(((int)(y / 8) + 1) * 128 + x + i, 1153))] |=
+            ASCII_Font[(int)(Size / 8) - 1][c * (Size / 2 * (int)(Size / 8)) + i + n * 8] >> (8 - (y % 8));
         }
-      }else{
-        for(int i=0; i<Size/2; i++){
-          GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] |= 0xFF<<(y%8);
-          GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] |= 0xFF>>(8-(y%8));
-          
-          GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] &= 
-            ~(ASCII_Font[(int)(Size/8)-1][c*(Size/2*(int)(Size/8))+i+n*8]<<(y%8));
-          
-          GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] &= 
-            ~(ASCII_Font[(int)(Size/8)-1][c*(Size/2*(int)(Size/8))+i+n*8]>>(8-(y%8)));
+      } else {
+        for(int i = 0; i < Size / 2; i++) {
+          GRAM[layer][Max(0, Min((int)(y / 8) * 128 + x + i, 1153))] |= 0xFF << (y % 8);
+          GRAM[layer][Max(0, Min(((int)(y / 8) + 1) * 128 + x + i, 1153))] |= 0xFF >> (8 - (y % 8));
+
+          GRAM[layer][Max(0, Min((int)(y / 8) * 128 + x + i, 1153))] &=
+            ~(ASCII_Font[(int)(Size / 8) - 1][c * (Size / 2 * (int)(Size / 8)) + i + n * 8] << (y % 8));
+
+          GRAM[layer][Max(0, Min(((int)(y / 8) + 1) * 128 + x + i, 1153))] &=
+            ~(ASCII_Font[(int)(Size / 8) - 1][c * (Size / 2 * (int)(Size / 8)) + i + n * 8] >> (8 - (y % 8)));
         }
       }
-      y+=8;
+      y += 8;
     }
-  }else{
-    for(int i=0;i<sizeof(Index)/sizeof("中");i++){
-      if(memcmp(chr,Index[i],sizeof("中")-1)==0){
-        OLED_ShowBMP(x,y-8,(uint8_t*)Font16[i],16,16,mode,layer);
+  } else {
+    for(int i = 0; i < sizeof(Index) / sizeof("中"); i++) {
+      if(memcmp(chr, Index[i], sizeof("中") - 1) == 0) {
+        OLED_ShowBMP(x, y - 8, (uint8_t*)Font16[i], 16, 16, mode, layer);
         break;
       }
     }
@@ -166,60 +166,65 @@ void OLED_ShowChar(int x,int y,char* chr,int Size,int mode,int layer) {
 参数: x(0~127), y(0~63): 起点坐标; *BMP:图片数组指针,BMP_Heigth: 图片高度;BMP_Width: 图片宽度;
       mode:0,反白显示;1,正常显示;layer: 要操作的层
 */
-void OLED_ShowBMP(int x,int y,uint8_t *BMP,int BMP_Heigth,int BMP_Width,int mode,int layer) {
-  y+=8;
-  for(int n=0;n<BMP_Heigth/8;n++){
-    if(mode == 1){
-      for(int i=0; i<BMP_Width; i++) {
-        GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] &= 0xFF>>(8-(y%8));
-        GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] &= 0xFF<<(y%8);
-        
-        GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] |= 
-          BMP[n*BMP_Width+i]<<(y%8);
-        
-        GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] |= 
-          BMP[n*BMP_Width+i]>>(8-(y%8));
+void OLED_ShowBMP(int x, int y, uint8_t* BMP, int BMP_Heigth, int BMP_Width, int mode, int layer) {
+  y += 8;
+  for(int n = 0; n < BMP_Heigth / 8; n++) {
+    if(mode == 1) {
+      for(int i = 0; i < BMP_Width; i++) {
+        GRAM[layer][Max(0, Min((int)(y / 8) * 128 + x + i, 1153))] &= 0xFF >> (8 - (y % 8));
+        GRAM[layer][Max(0, Min(((int)(y / 8) + 1) * 128 + x + i, 1153))] &= 0xFF << (y % 8);
+
+        GRAM[layer][Max(0, Min((int)(y / 8) * 128 + x + i, 1153))] |=
+          BMP[n * BMP_Width + i] << (y % 8);
+
+        GRAM[layer][Max(0, Min(((int)(y / 8) + 1) * 128 + x + i, 1153))] |=
+          BMP[n * BMP_Width + i] >> (8 - (y % 8));
       }
-    }else{
-      for(int i=0; i<BMP_Width; i++) {
-        GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] |= 0xFF<<(y%8);
-        GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] |= 0xFF>>(8-(y%8));
-        
-        GRAM[layer][Max(0,Min((int)(y/8)*128 + x + i,1153))] &= 
-          ~(BMP[n*BMP_Width+i]<<(y%8));
-        
-        GRAM[layer][Max(0,Min(((int)(y/8)+1)*128 + x + i,1153))] &= 
-          ~(BMP[n*BMP_Width+i]>>(8-(y%8)));
+    } else {
+      for(int i = 0; i < BMP_Width; i++) {
+        GRAM[layer][Max(0, Min((int)(y / 8) * 128 + x + i, 1153))] |= 0xFF << (y % 8);
+        GRAM[layer][Max(0, Min(((int)(y / 8) + 1) * 128 + x + i, 1153))] |= 0xFF >> (8 - (y % 8));
+
+        GRAM[layer][Max(0, Min((int)(y / 8) * 128 + x + i, 1153))] &=
+          ~(BMP[n * BMP_Width + i] << (y % 8));
+
+        GRAM[layer][Max(0, Min(((int)(y / 8) + 1) * 128 + x + i, 1153))] &=
+          ~(BMP[n * BMP_Width + i] >> (8 - (y % 8)));
       }
     }
-    y+=8;
+    y += 8;
   }
 }
 
 //画四分之一圆
-void OLED_DrawQuarterRound(int xo,int yo,int r,int width,int quarter,uint32_t color,int layer){
+void OLED_DrawQuarterRound(int xo, int yo, int r, int width, int quarter, uint32_t color, int layer) {
   int xs = Max(xo, 0);
   int xe = Min(xo + r + 1, 127);
   int ys = Max(yo, 0);
   int ye = Min(yo + r + 1, 63);
-  
-  for (int i=ys;i<ye;i++){
-    for (int t=xs-ys+i;t<xe;t++){
-      if (fabs(powf(xo-t,2)+powf(yo-i,2))<=powf(r,2)){
-        switch(quarter){
-          case 1:OLED_DrawPoint(t,2*ys-i,color,layer);
-            OLED_DrawPoint(xs+i-ys,ys+xs-t,color,layer);
-            break;
-          case 2:OLED_DrawPoint(2*xs-t,2*ys-i,color,layer);
-            OLED_DrawPoint(xs+ys-i,ys+xs-t,color,layer);
-            break;
-          case 3:OLED_DrawPoint(2*xs-t,i,color,layer);
-            OLED_DrawPoint(xs+ys-i,ys+t-xs,color,layer);
-            break;
-          case 4:OLED_DrawPoint(t,i,color,layer);
-            OLED_DrawPoint(xs+i-ys,ys+t-xs,color,layer);
-            break;
-          default:break;
+
+  for(int i = ys; i < ye; i++) {
+    for(int t = xs - ys + i; t < xe; t++) {
+      if(fabs(powf(xo - t, 2) + powf(yo - i, 2)) <= powf(r, 2)) {
+        switch(quarter) {
+        case 1:
+          OLED_DrawPoint(t, 2 * ys - i, color, layer);
+          OLED_DrawPoint(xs + i - ys, ys + xs - t, color, layer);
+          break;
+        case 2:
+          OLED_DrawPoint(2 * xs - t, 2 * ys - i, color, layer);
+          OLED_DrawPoint(xs + ys - i, ys + xs - t, color, layer);
+          break;
+        case 3:
+          OLED_DrawPoint(2 * xs - t, i, color, layer);
+          OLED_DrawPoint(xs + ys - i, ys + t - xs, color, layer);
+          break;
+        case 4:
+          OLED_DrawPoint(t, i, color, layer);
+          OLED_DrawPoint(xs + i - ys, ys + t - xs, color, layer);
+          break;
+        default:
+          break;
         }
       }
     }
@@ -227,69 +232,69 @@ void OLED_DrawQuarterRound(int xo,int yo,int r,int width,int quarter,uint32_t co
 }
 
 //反色显示
-void OLED_Negation(int ixs,int iys,int ixe,int iye,int mode){
-  int xs=Min(ixs,ixe),ys=Min(iys,iye),
-      xe=Max(ixs,ixe),ye=Max(iys,iye);      //重新整理起始点和结束点
-  
+void OLED_Negation(int ixs, int iys, int ixe, int iye, int mode) {
+  int xs = Min(ixs, ixe), ys = Min(iys, iye),
+      xe = Max(ixs, ixe), ye = Max(iys, iye); //重新整理起始点和结束点
+
   OLED_Clear(OLED_IMAGE_LAYER);
-  
-  if(mode){       //圆角
-    OLED_DrawQuarterRound(xs+2,ys+2,2,1,2,1,OLED_IMAGE_LAYER);
-    OLED_DrawQuarterRound(xe-2,ys+2,2,1,1,1,OLED_IMAGE_LAYER);
-    OLED_DrawQuarterRound(xs+2,ye-2,2,1,3,1,OLED_IMAGE_LAYER);
-    OLED_DrawQuarterRound(xe-2,ye-2,2,1,4,1,OLED_IMAGE_LAYER);
-    
-    for(int i=ys+2;i<=ye-2;i++){
-      for(int t=xs;t<=xe;t++){
-        OLED_DrawPoint(t,i,1,OLED_IMAGE_LAYER);
+
+  if(mode) {      //圆角
+    OLED_DrawQuarterRound(xs + 2, ys + 2, 2, 1, 2, 1, OLED_IMAGE_LAYER);
+    OLED_DrawQuarterRound(xe - 2, ys + 2, 2, 1, 1, 1, OLED_IMAGE_LAYER);
+    OLED_DrawQuarterRound(xs + 2, ye - 2, 2, 1, 3, 1, OLED_IMAGE_LAYER);
+    OLED_DrawQuarterRound(xe - 2, ye - 2, 2, 1, 4, 1, OLED_IMAGE_LAYER);
+
+    for(int i = ys + 2; i <= ye - 2; i++) {
+      for(int t = xs; t <= xe; t++) {
+        OLED_DrawPoint(t, i, 1, OLED_IMAGE_LAYER);
       }
     }
-    for(int i=ys;i<=ys+2;i++){
-      for(int t=xs+2;t<=xe-2;t++){
-        OLED_DrawPoint(t,i,1,OLED_IMAGE_LAYER);
+    for(int i = ys; i <= ys + 2; i++) {
+      for(int t = xs + 2; t <= xe - 2; t++) {
+        OLED_DrawPoint(t, i, 1, OLED_IMAGE_LAYER);
       }
     }
-    for(int i=ye-2;i<=ye;i++){
-      for(int t=xs+2;t<=xe-2;t++){
-        OLED_DrawPoint(t,i,1,OLED_IMAGE_LAYER);
+    for(int i = ye - 2; i <= ye; i++) {
+      for(int t = xs + 2; t <= xe - 2; t++) {
+        OLED_DrawPoint(t, i, 1, OLED_IMAGE_LAYER);
       }
     }
-    
-  }else{
-    for(int i=ys;i<=ye;i++){
-      for(int t=xs;t<=xe;t++){
-        OLED_DrawPoint(t,i,1,OLED_IMAGE_LAYER);
+
+  } else {
+    for(int i = ys; i <= ye; i++) {
+      for(int t = xs; t <= xe; t++) {
+        OLED_DrawPoint(t, i, 1, OLED_IMAGE_LAYER);
       }
     }
   }
-  
-  for(int i=0;i<1024;i++){
-    GRAM[OLED_DATA_LAYER][i+128] ^= GRAM[OLED_IMAGE_LAYER][i+128];
+
+  for(int i = 0; i < 1024; i++) {
+    GRAM[OLED_DATA_LAYER][i + 128] ^= GRAM[OLED_IMAGE_LAYER][i + 128];
   }
   OLED_Clear(OLED_IMAGE_LAYER);
 }
 
 //或运算更新显存
-void OLED_UpDateToGRAM(uint8_t Data[1024],int layer){
-  for(int i=0;i<1024;i++){
-    GRAM[layer][i+128]|=Data[i];
+void OLED_UpDateToGRAM(uint8_t Data[1024], int layer) {
+  for(int i = 0; i < 1024; i++) {
+    GRAM[layer][i + 128] |= Data[i];
   }
 }
 
 //获取显存
-void OLED_GetGRAM(uint8_t *Data,int layer){
-  memcpy(Data,GRAM[layer]+128,1024);
+void OLED_GetGRAM(uint8_t* Data, int layer) {
+  memcpy(Data, GRAM[layer] + 128, 1024);
 }
 
 //光栅运算
-void OLED_Grating_Count(){
-  for(int i=0;i<1024;i++){
-    GRAM[OLED_DATA_LAYER][i+128] &= ~GRAM[OLED_GRATING_LAYER][i+128];
-    GRAM[OLED_DATA_LAYER][i+128] |= GRAM[OLED_IMAGE_LAYER][i+128]&GRAM[OLED_GRATING_LAYER][i+128];
+void OLED_Grating_Count() {
+  for(int i = 0; i < 1024; i++) {
+    GRAM[OLED_DATA_LAYER][i + 128] &= ~GRAM[OLED_GRATING_LAYER][i + 128];
+    GRAM[OLED_DATA_LAYER][i + 128] |= GRAM[OLED_IMAGE_LAYER][i + 128] & GRAM[OLED_GRATING_LAYER][i + 128];
   }
 }
 
 //发送显存
 void OLED_GRAM_Transmit(void) {
-  OLED_WR_Data(GRAM[OLED_DATA_LAYER]+128);
+  OLED_WR_Data(GRAM[OLED_DATA_LAYER] + 128);
 }
